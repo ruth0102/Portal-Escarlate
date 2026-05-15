33e617770f53ca49cd72ae825e0ea736ce8deeaf
luigicollesi <luigicollesi@gmail.com>
2026-05-14 23:06:41 -0300

    ajustando vite.config do front
    O vit configue não estava diferenciando dns de front e back end. o que criava erros de roas em algumas situações.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,27 @@
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 
-const rootEnvDir = fileURLToPath(new URL('..', import.meta.url))
+const rootEnvDir = new URL('..', import.meta.url).pathname
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, rootEnvDir, '')
   const apiUrl = env.VITE_API_URL || 'http://127.0.0.1:3000'
+  const allowedHosts = ['portalescarlate.com.br', 'api.portalescarlate.com.br']
 
   return {
     plugins: [react()],
+    envDir: rootEnvDir,
     server: {
+      allowedHosts,
       proxy: {
         '/api': {
           target: apiUrl,
           changeOrigin: true,
         },
       },
+    },
+    preview: {
+      allowedHosts,
     },
   }
 })
